b87028d02b881c517ebf51588d6efa1f10cf3996
Aditya Bijapurkar <aditya.bijapurkar@aidash.com>
2025-10-20 06:56:16 +0530

    implemented flyway db migration

---
+++ src/main/resources/db/migration/V1__init_baseline.sql
@@ -0,0 +1,3 @@
+-- This is a dummy baseline migration for Flyway.
+-- Real migrations for this service start from V2__.
+SELECT 1;
--- src/main/resources/db/migration/V2__create_schema.sql
+++ src/main/resources/db/migration/V2__create_schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE status (
 	id UUID PRIMARY KEY,
 	title TEXT
