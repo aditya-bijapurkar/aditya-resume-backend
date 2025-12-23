4453c036f2248609aba2ffafed3e844df7564bd5
Aditya Bijapurkar <aditya.bijapurkar@aidash.com>
2025-12-24 01:52:02 +0530

    jsonignoreproperties error fix

--- src/main/java/com/example/aditya_resume_backend/core/port/dto/ScheduledMeetingDetailsDTO.java
+++ src/main/java/com/example/aditya_resume_backend/core/port/dto/ScheduledMeetingDetailsDTO.java
@@ -1,11 +1,17 @@
 package com.example.aditya_resume_backend.core.port.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class ScheduledMeetingDetailsDTO {
 
     @JsonProperty("start_url")
